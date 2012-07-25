1c2f1536ce9a9d3d8dc13ea2707a245f4b4f9320
Eric Bottard <eric.bottard+github@gmail.com>
2012-07-25 15:04:02 +0200

    Merged from upstream

--- cfdroid/src/org/cloudfoundry/android/cfdroid/applications/ApplicationsActivity.java
+++ cfdroid/src/org/cloudfoundry/android/cfdroid/applications/ApplicationsActivity.java
@@ -4,12 +4,19 @@ import org.cloudfoundry.android.cfdroid.R;
 import org.cloudfoundry.android.cfdroid.support.masterdetail.MasterDetailActivity;
 import org.cloudfoundry.client.lib.CloudApplication;
 
+import android.os.Bundle;
+
 import roboguice.inject.ContentView;
 
 @ContentView(R.layout.applications)
 public class ApplicationsActivity
 		extends
 		MasterDetailActivity<CloudApplication, ApplicationsListFragment, ApplicationDetailTabs> {
+	@Override
+	public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		getActionBar().setDisplayShowTitleEnabled(false);
+	}
 
 	@Override
 	protected ApplicationsListFragment makeLeftFragment() {
@@ -18,8 +25,9 @@ public class ApplicationsActivity
 
 	@Override
 	protected ApplicationDetailTabs makeRightFragment() {
+		//return new ApplicationDetailViewPager();
 		return new ApplicationDetailTabs();
-	}
+	}   
 
 	
 }
