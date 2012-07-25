f33d95b399f1307824fc68a953447ef86b133d61
Rajdeep Dua <Rajdeep Dua>
2012-07-25 13:03:01 +0530

    Removed title from action bar, Changed App Logo Changed color of the App List items

--- cfdroid/src/org/cloudfoundry/android/cfdroid/applications/ApplicationsActivity.java
+++ cfdroid/src/org/cloudfoundry/android/cfdroid/applications/ApplicationsActivity.java
@@ -4,12 +4,19 @@ import org.cloudfoundry.android.cfdroid.R;
 import org.cloudfoundry.android.cfdroid.support.masterdetail.MasterDetailActivity;
 import org.cloudfoundry.client.lib.CloudApplication;
 
+import android.os.Bundle;
+
 import roboguice.inject.ContentView;
 
 @ContentView(R.layout.applications)
 public class ApplicationsActivity
 		extends
 		MasterDetailActivity<CloudApplication, ApplicationsListFragment, ApplicationDetailTabs> {
+	@Override
+	public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		getActionBar().setDisplayShowTitleEnabled(false);
+	}
 
 	@Override
 	protected ApplicationsListFragment makeLeftFragment() {
@@ -20,7 +27,7 @@ public class ApplicationsActivity
 	protected ApplicationDetailTabs makeRightFragment() {
 		//return new ApplicationDetailViewPager();
 		return new ApplicationDetailTabs();
-	}
+	}   
 
 	
 }
